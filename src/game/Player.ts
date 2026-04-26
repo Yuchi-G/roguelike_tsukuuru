89135ac89d1c500ca22b236b9bd61dd1a08598ef
yamada yuichi <yamadayuichi.1016@gmail.com>
2026-04-26 22:36:22 +0900

    add level

--- src/game/Player.ts
+++ src/game/Player.ts
@@ -6,7 +6,28 @@ import { Actor } from "../engine/Entity";
 
 /** プレイヤーキャラクター。入力による移動や攻撃はGame側で処理する。 */
 export class Player extends Actor {
+  public level = 1;
+  public exp = 0;
+  public nextLevelExp = 10;
+
   constructor(x: number, y: number) {
     super(x, y, "@", "#f5f0d0", "プレイヤー", 30, 30, 5);
+  }
+
+  /** 経験値が次のレベルに届いていれば、ステータスを伸ばしてレベルアップする。 */
+  checkLevelUp(): number {
+    let levelUps = 0;
+
+    while (this.exp >= this.nextLevelExp) {
+      this.exp -= this.nextLevelExp;
+      this.level += 1;
+      this.nextLevelExp = Math.floor(this.nextLevelExp * 1.5);
+      this.maxHp += 5;
+      this.hp = this.maxHp;
+      this.attackPower += 2;
+      levelUps += 1;
+    }
+
+    return levelUps;
   }
 }
